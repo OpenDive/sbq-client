d325c5b8fef21bd47b42cb087306890fcab04ee8
Irvin <irvsteve@gmail.com>
2024-05-31 13:43:15 -0500

    Add ZK Login implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-// import { useState, Fragment } from 'react'
-import { Fragment } from 'react'
+import { useState, useEffect, Fragment } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import {
+  CLIENT_ID,
+  FULLNODE_URL,
+  KEY_PAIR_SESSION_STORAGE_KEY,
+  MAX_EPOCH_LOCAL_STORAGE_KEY,
+  RANDOMNESS_SESSION_STORAGE_KEY,
+  REDIRECT_URI
+} from "./constant.tsx";
+import {
+  generateNonce,
+  generateRandomness,
+} from "@mysten/zklogin";
+import { SuiClient } from "@mysten/sui.js/client";
+
 import { Unity, useUnityContext } from "react-unity-webgl";
+import { PublicKey } from '@mysten/sui/cryptography';
 
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [nonce, setNonce] = useState("");
+  const [ephemeralKeyPair, setEphemeralKeyPair] = useState<Ed25519Keypair>();
+  const [maxEpoch, setMaxEpoch] = useState(0);
+  const [randomness, setRandomness] = useState("");
 
   const { unityProvider, loadingProgression, isLoaded, sendMessage } = useUnityContext({
     loaderUrl: "public/UnityWeb3-WebGL.loader.js",
@@ -21,8 +40,68 @@ function App() {
     sendMessage("Web3Controller", "OnZkLogin", "true");
   }
 
+  const suiClient = new SuiClient({ url: FULLNODE_URL });
+
+  // Get Randomness
+  useEffect(() => {
+    const randomness = generateRandomness();
+    window.sessionStorage.setItem(
+      RANDOMNESS_SESSION_STORAGE_KEY,
+      randomness
+    );
+    setRandomness(randomness);
+  }, []);
+
+  // Get Epoch
+  useEffect(() => {
+    async function epochFetch() {
+      const { epoch } = await suiClient.getLatestSuiSystemState();
+      window.localStorage.setItem(
+        MAX_EPOCH_LOCAL_STORAGE_KEY,
+        String(Number(epoch) + 10)
+      );
+      setMaxEpoch(Number(epoch) + 10);
+    }
+    epochFetch();
+  }, []);
+
+  // Generate Ephemeral Keypair
+  useEffect(() => {
+    const ephemeralKeyPair = Ed25519Keypair.generate();
+    window.sessionStorage.setItem(
+      KEY_PAIR_SESSION_STORAGE_KEY,
+      ephemeralKeyPair.export().privateKey
+    );
+    setEphemeralKeyPair(ephemeralKeyPair);
+  }, []);
+
+  // Generate Nonce
+  useEffect(() => {
+    if (!ephemeralKeyPair) { return; }
+    const nonce = generateNonce(
+      ephemeralKeyPair.getPublicKey(),
+      maxEpoch,
+      randomness
+    );
+    setNonce(nonce);
+  }, [ephemeralKeyPair, maxEpoch, randomness]);
+
+  function loginZK() {
+      const params = new URLSearchParams({
+        client_id: CLIENT_ID,
+        redirect_uri: REDIRECT_URI,
+        response_type: "id_token",
+        scope: "openid",
+        nonce: nonce,
+      });
+      const loginURL = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
+      window.location.replace(loginURL);
+  }
+
   return (
     <Fragment>
+      <button onClick={loginZK}>SIGN IN WITH GOOGLE</button>
+
       <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
       <Unity 
         unityProvider={unityProvider} 
---
+++ src/constant.tsx
@@ -0,0 +1,24 @@
+export const FULLNODE_URL = "https://fullnode.devnet.sui.io";
+
+export const CLIENT_ID =
+  "132494794905-3g20nk4imhr2tn2ac84bevfts8tl2r7q.apps.googleusercontent.com";
+
+export const REDIRECT_URI = "http://localhost:3000/oauth/callback";
+
+export const SUI_DEVNET_FAUCET = 'https://faucet.devnet.sui.io/gas'
+
+export const SUI_PROVER_DEV_ENDPOINT = 'https://prover-dev.mystenlabs.com/v1'
+
+export const KEY_PAIR_SESSION_STORAGE_KEY = "demo_ephemeral_key_pair";
+
+export const USER_SALT_LOCAL_STORAGE_KEY = "demo_user_salt_key_pair";
+
+export const RANDOMNESS_SESSION_STORAGE_KEY = "demo_randomness_key_pair";
+
+export const MAX_EPOCH_LOCAL_STORAGE_KEY = "demo_max_epoch_key_pair";
+
+
+export const STEPS_LABELS_TRANS_KEY = ['16e758e8', '9b8b5398', '8adf5b45', '8b72e7cd', '66f6b490', 'af802c7a', 'c649dd70',]
+
+
+export const STEPS_DESC = ['ephemeralKeyPair', '47b83f4e', 'fb399be8', '0a710e64', '32255d31', '8f2433d9']
