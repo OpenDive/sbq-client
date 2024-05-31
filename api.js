1df5fc71bda72d999904b455cc22f135d16ad2a8
Irvin <irvsteve@gmail.com>
2024-05-31 06:44:39 -0500

    Update vite API support

--- api.js
+++ api.js
@@ -1,9 +1,29 @@
+import 'dotenv/config'
 import express from 'express';
+
+import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
+import { KioskClient, Network } from '@mysten/kiosk';
+
+const options = {
+    method: 'GET', 
+    headers: {
+        accept: 'application/json', 'x-api-key': process.env.INDEXER_API
+    }
+};
+const addr = "0x6045d15865d74a53dc42e481280190aec7d06463f40088acf49ea4585acc29e2"
+const suiFrensPackage = "0xee496a0cc04d06a345982ba6697c90c619020de9e274408c7819f787ff66e1a1"
 
 const app = express();
 
+
+// localhost:8280/api/?address=0x6045d15865d74a53dc42e481280190aec7d06463f40088acf49ea4585acc29e2
+app.get("/api/getSuiFrens", (req, res) => {
+    const address = req.query.address
+    res.json({ address: address });
+});
+  
 app.get("/api/hello", (req, res) => {
-  res.json({ hello: "world" });
+    res.json({ hello: "world" });
 });
 
 export const handler = app;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mix from 'vite-plugin-mix'
+// import mix from 'vite-plugin-mix'
+import mix$ from 'vite-plugin-mix'
 
+const mix = mix$.default ?? mix$
 
 // https://vitejs.dev/config/
 export default defineConfig({
